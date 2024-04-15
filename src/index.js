4078575398e881ac2062775b63eb06ff973b0385
rockr <hattarakiravi8@gmail.com>
2024-04-15 14:18:23 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-      <Provider store={store}>
+      // <Provider store={store}>
         <App />
-      </Provider>
+      // </Provider>
 
 );
 
